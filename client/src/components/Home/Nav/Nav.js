c494edfdbc75fb1a99d5640b9077863c6756a52a
Terron Johnson <tmj72386@gmail.com>
2019-12-01 09:54:10 -0500

    Made Nav Bar Mobil Friendly which was a pain in the ass

--- client/src/components/Home/Nav/Nav.js
+++ client/src/components/Home/Nav/Nav.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-
+import { Nav, Navbar, NavDropdown, Form, FormControl } from "react-bootstrap";
 
 class NavBar extends Component {
   constructor(props) {
@@ -33,81 +33,82 @@ class NavBar extends Component {
         return;
       case false:
         return [
-          <Link className="nav-link" to="/post-studio">
+          <Link to="/post-studio">
             <i className="fa fa-plus"></i>Add Your Studio
           </Link>,
-          <Link className="nav-link" to="/sign-up">
-            Sign Up/Login
-          </Link>
+          <Link to="/sign-up">Sign Up/Login</Link>
         ].map((value, i) => {
-          return (
-            <li key={i} className="nav-item">
-              {value}
-            </li>
-          );
+          return <li key={i}>{value}</li>;
         });
       default:
         return [
-          <Link className="nav-link" to="/post-studio">
+          <Link to="/post-studio">
             <i className="fa fa-plus"></i> Add Your Studio
           </Link>,
 
-          <Link to="/userprofile" className="nav-link">
-            {this.props.auth.name}'s' Profile
-          </Link>,
-          <a className="nav-link" href="/api/logout">
-            Logout
-          </a>,
-          
+          <Link to="/userprofile">{this.props.auth.name}'s' Profile</Link>,
+          <a href="/api/logout">Logout</a>
         ].map((value, i) => {
-          return (
-            <li key={i} className="nav-item">
-              {value}
-            </li>
-          );
+          return <Nav.Link key={i}>{value}</Nav.Link>;
         });
     }
   }
 
   render() {
     let { revealSearch } = this.props;
+    let { width } = this.state;
     return (
-      <div className="main-header-area">
-        <div className="classy-nav-container breakpoint-off">
-          <div className="container">
-
-            <nav
-              className="classy-navbar justify-content-between"
-              id="robertoNav"
-            >
-              <Link className="nav-brand" to="/">
-                Studio Hunt
-              </Link>
-
-              <div className="classynav">
-                <ul id="nav">{this.renderContent()}</ul>
-
-                <div className="search-btn ml-4">
-                  <i className="fa fa-search" onClick={revealSearch}></i>
-                </div>
-
-                <div className="book-now-btn ml-3 ml-lg-5">
-                  <Link to="/search-studio">
-                    Book Now{" "}
-                    <i
-                      className="fa fa-long-arrow-right"
-                      aria-hidden="true"
-                    ></i>
-                  </Link>
-                </div>
-              </div>
-            </nav>
-          </div>
+      <div className={`main-header-area`}>
+        <div className="container">
+          <Navbar
+            expand="lg"
+            style={width > 1000 ? styles.navStyles : styles.navPad}
+            className="justify-content-between"
+            id="robertoNav"
+          >
+            <Navbar.Brand>
+              <Link to="/">StudioHunt</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">{this.renderContent()}</Nav>
+              <Nav.Link>
+                <i className="fa fa-search" onClick={revealSearch}></i>
+              </Nav.Link>
+              <Nav.Link>
+                <Link style={styles.bookStyles} to="/search-studio">
+                  Book Now{" "}
+                  <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+                </Link>
+              </Nav.Link>
+            </Navbar.Collapse>
+          </Navbar>
         </div>
       </div>
     );
   }
 }
+
+const styles = {
+  bookStyles: {
+    padding: "0 30px",
+    backgroundColor: "#1cc3b2",
+    height: "80px",
+    lineHeight: "80px",
+    textAlign: "center",
+    color: "#ffffff",
+    fontSize: "16px",
+    padding: "30px"
+  },
+  navStyles: {
+    height: "80px",
+    padding: "0"
+  },
+  navPad: {
+    height: "auto",
+    padding: "20"
+  }
+};
 
 function mapStateToProps({ auth }) {
   //State from reducers/index.js file  gets passed to header component as props
